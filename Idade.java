1131381a777117538f58564de55ce29535fba021
Lucas Soares Santos (Padrinho) <99371201+LucasSoares482@users.noreply.github.com>
2026-04-06 10:06:55 -0300

    att
    atualização idade

--- Idade.java
+++ Idade.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Idade {
     public static void main(String[] args) {
 
-        int idade = 25;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
 
         if (idade <= 12) {
             System.out.println("Criança");
